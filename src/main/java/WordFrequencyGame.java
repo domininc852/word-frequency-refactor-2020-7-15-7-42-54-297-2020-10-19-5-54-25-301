71244663878c11a8f6478d389dfa0fbefa527bbc
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-11-30 17:14:39 +0800

    [WordFrequency][Dominic Leung] refactor: Extract constant for magic string

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,13 +5,17 @@ import java.util.Map;
 import java.util.StringJoiner;
 
 public class WordFrequencyGame {
+
+    private static final String WHITE_SPACE_REGEX = "\\s+";
+    private static final String NEW_LINE = "\n";
+
     public String getResult(String inputStr) {
-        if (inputStr.split("\\s+").length == 1) {
+        if (inputStr.split(WHITE_SPACE_REGEX).length == 1) {
             return inputStr + " 1";
         } else {
             try {
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] arr = inputStr.split(WHITE_SPACE_REGEX);
 
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
@@ -28,7 +32,7 @@ public class WordFrequencyGame {
                 }
                 inputList = list;
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-                StringJoiner joiner = new StringJoiner("\n");
+                StringJoiner joiner = new StringJoiner(NEW_LINE);
                 for (Input w : inputList) {
                     String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
